108f16cca1aec567878cfecdc61f30468cd4701d
tpb22 <tpb22@jupyterhub.crc.pitt.edu>
2019-10-02 12:32:19 -0400

    Added multiplication, division, and name input to calc

--- calculator.py
+++ calculator.py
@@ -1,12 +1,17 @@
-print("Welcome to your personal calculator")
+name = raw_input("What is your name? ")
+print("Welcome to your personal calculator " + name )
 x = int(input("What is your first number: "))
 y = int(input("What is your second number: "))
-operation = input("What is the operation (Enter Symbol) ")
+operation = raw_input("What is the operation (Enter Symbol) ")
 
 if operation == "+" :
 	print(x + y)
 
 elif operation == "-" :
 	print(x - y)
+elif operation == "*" :
+	print(x * y)
+elif operation == "/" :
+	print(x/y)
 else:
 	print("Error invalid operator")
